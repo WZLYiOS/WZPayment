ab08878d15480e186b045f0429043af0a36a1415
qiuqixiangT <995386924@qq.com>
2024-04-17 17:01:19 +0800

    -feat：错误码定义

--- WZPayment/Classes/WZPaymentError.swift
+++ WZPayment/Classes/WZPaymentError.swift
@@ -11,6 +11,25 @@
 import Foundation
 import StoreKit
 
+/// 错误码
+public enum WZPaymentCode: Int {
+case orderNil = 120001 // 订单id返回空
+case productNil = 120002 // 产品id返回空
+case orderDb = 120003 // 订单db保存失败
+case NoCanPay = 120004 // 请到系统设置，开启苹果支付功能
+case history = 120005 // 发起内购失败：当前有历史订单未补,已上报中...，请稍后再购买
+case dbReceipt = 120006 // "内购支付失败：支付凭证存入db失败"
+case notproduct = 120007 // "发起内购失败：未获取到该商品"
+case other = 120008     // 发起内购失败：\(text)
+case appleUnknown = 120009 // 苹果服务器：未知错误，请联系客服
+case paymentCancelled = 120010 // "苹果服务器：购买失败，您取消了付款"
+case cloudServiceRevoked = 120011 // "苹果服务器：您已撤消使用此云服务的权限"
+case paymentInvalid = 120012 // 苹果服务器：App Store无法识别付款参数
+case paymentNotAllowed = 120013// 苹果服务器：请开启授权付款权限
+case storeProductNotAvailable = 120014 // 苹果服务器：所请求的产品在商店中不可用。
+case cloudServiceNetworkConnectionFailed = 120015 // 苹果服务器：设备无法连接到网络
+}
+
 /// MARK - 错误码
 public enum WZPaymentError {
 case orderNil
@@ -25,14 +44,14 @@ case custom(String)
     /// 错误
     var err: NSError {
         switch self {
-        case .orderNil: return NSError(domain: "发起内购失败：订单id返回空，请联系客服", code: 120001)
-        case .productNil: return NSError(domain: "发起内购失败：产品id返回空，请联系客服", code: 120002)
-        case .orderDb: return NSError(domain: "发起内购失败：订单db保存失败，无法继续购买，请联系客服", code: 120003)
-        case .NoCanPay: return NSError(domain: "发起内购失败：请到系统设置，开启苹果支付功能", code: 120004)
-        case .history: return NSError(domain: "发起内购失败：当前有历史订单未补,已上报中...，请稍后再购买", code: 120005)
-        case .db: return NSError(domain: "内购支付失败：支付凭证存入db失败", code: 120006)
-        case .notproduct: return NSError(domain: "发起内购失败：未获取到该商品", code: 120007)
-        case let .custom(text): return NSError(domain: "发起内购失败：\(text)", code: 120008)
+        case .orderNil: return NSError(domain: "发起内购失败：订单id返回空，请联系客服", code: WZPaymentCode.orderNil.rawValue)
+        case .productNil: return NSError(domain: "发起内购失败：产品id返回空，请联系客服", code: WZPaymentCode.productNil.rawValue)
+        case .orderDb: return NSError(domain: "发起内购失败：订单db保存失败，无法继续购买，请联系客服", code: WZPaymentCode.orderDb.rawValue)
+        case .NoCanPay: return NSError(domain: "发起内购失败：请到系统设置，开启苹果支付功能", code: WZPaymentCode.NoCanPay.rawValue)
+        case .history: return NSError(domain: "发起内购失败：当前有历史订单未补,已上报中...，请稍后再购买", code: WZPaymentCode.history.rawValue)
+        case .db: return NSError(domain: "内购支付失败：支付凭证存入db失败", code: WZPaymentCode.dbReceipt.rawValue)
+        case .notproduct: return NSError(domain: "发起内购失败：未获取到该商品", code: WZPaymentCode.notproduct.rawValue)
+        case let .custom(text): return NSError(domain: "发起内购失败：\(text)", code: WZPaymentCode.other.rawValue)
         }
     }
 }
@@ -45,23 +64,45 @@ extension SKError {
     var wzError: NSError {
         switch self.code {
         case .unknown:
-            return NSError(domain: "苹果服务器：未知错误，请联系客服", code: 120009, userInfo: nil)
+            return NSError(domain: self.localizedDescription, code: WZPaymentCode.appleUnknown.rawValue, userInfo: nil)
         case .paymentCancelled:
-            return NSError(domain: "苹果服务器：购买失败，您取消了付款", code: 120010, userInfo: nil)
+            return NSError(domain: self.localizedDescription, code: WZPaymentCode.paymentCancelled.rawValue, userInfo: nil)
         case .cloudServiceRevoked:
-            return NSError(domain: "苹果服务器：您已撤消使用此云服务的权限", code: 120011, userInfo: nil)
+            return NSError(domain: self.localizedDescription, code: WZPaymentCode.cloudServiceRevoked.rawValue, userInfo: nil)
         case .paymentInvalid:
-            return NSError(domain: "苹果服务器：App Store无法识别付款参数", code: 120012, userInfo: nil)
+            return NSError(domain: self.localizedDescription, code: WZPaymentCode.paymentInvalid.rawValue, userInfo: nil)
         case .paymentNotAllowed:
-            return NSError(domain: "苹果服务器：请开启授权付款权限", code: 120013, userInfo: nil)
+            return NSError(domain: self.localizedDescription, code: WZPaymentCode.paymentNotAllowed.rawValue, userInfo: nil)
         case .storeProductNotAvailable:
-            return NSError(domain: "苹果服务器：所请求的产品在商店中不可用。", code: 120014, userInfo: nil)
+            return NSError(domain: self.localizedDescription, code: WZPaymentCode.storeProductNotAvailable.rawValue, userInfo: nil)
         case .cloudServiceNetworkConnectionFailed:
-            return NSError(domain: "苹果服务器：设备无法连接到网络。", code: 120015, userInfo: nil)
+            return NSError(domain: self.localizedDescription, code: WZPaymentCode.cloudServiceNetworkConnectionFailed.rawValue, userInfo: nil)
         default:
-            return NSError(domain: "苹果服务器：未知错误", code: 12001016, userInfo: nil)
+            return NSError(domain: self.localizedDescription, code: WZPaymentCode.appleUnknown.rawValue, userInfo: nil)
         }
     }
+    
+//    /// 苹果服务器返回错误
+//    var wzError: NSError {
+//        switch self.code {
+//        case .unknown:
+//            return NSError(domain: "苹果服务器：未知错误，请联系客服", code: 120009, userInfo: nil)
+//        case .paymentCancelled:
+//            return NSError(domain: "苹果服务器：购买失败，您取消了付款", code: 120010, userInfo: nil)
+//        case .cloudServiceRevoked:
+//            return NSError(domain: "苹果服务器：您已撤消使用此云服务的权限", code: 120011, userInfo: nil)
+//        case .paymentInvalid:
+//            return NSError(domain: "苹果服务器：App Store无法识别付款参数", code: 120012, userInfo: nil)
+//        case .paymentNotAllowed:
+//            return NSError(domain: "苹果服务器：请开启授权付款权限", code: 120013, userInfo: nil)
+//        case .storeProductNotAvailable:
+//            return NSError(domain: "苹果服务器：所请求的产品在商店中不可用。", code: 120014, userInfo: nil)
+//        case .cloudServiceNetworkConnectionFailed:
+//            return NSError(domain: "苹果服务器：设备无法连接到网络。", code: 120015, userInfo: nil)
+//        default:
+//            return NSError(domain: "苹果服务器：未知错误", code: 12001016, userInfo: nil)
+//        }
+//    }
 }
 
 /// 扩展错误码
